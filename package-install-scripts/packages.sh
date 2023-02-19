54d2f0e9d3fd49d254c5982660450721e136514e
PJDuszynski <scottduszy@proton.me>
2023-02-18 18:32:33 -0600

    move scripts out of dotfiles

---
+++ package-install-scripts/packages.sh
@@ -0,0 +1,41 @@
+export AUR_packages=(
+    # Productivity Tools
+    betterbird-bin
+    todotxt
+    tutanota-desktop
+    electronmail-bin
+    zsh-theme-powerlevel10k-git
+)
+
+export pacman_packages=(
+    # Browser
+    vivaldi
+    gnupg
+    # Go
+    go
+    shfmt
+    #misc
+    neofetch
+    lsd
+    jq
+    tealdeer
+    zoxide
+    zsh
+    rofi
+    yadm
+
+)
+
+export pip_packages=(
+    pipx
+)
+
+export pipx_packages=(
+    black
+    pylint
+    flake8
+    radon
+    poetry
+    pytest
+    radian
+)
